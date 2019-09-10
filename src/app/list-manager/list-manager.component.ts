73dfd13832e8364b8133ad36798e290d2f9f3613
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-09-10 17:50:43 +0200

    Update todo-item component by adding todo's removing and todo's checking functionalities

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'app-list-manager',
@@ -9,7 +10,12 @@ import { TodoItem } from '../interfaces/todo-item';
 
       <ul>
         <li *ngFor="let todoItem of todoList">
-          <app-todo-item [item]="todoItem"></app-todo-item>
+          <app-todo-item
+            [item]="todoItem"
+            (remove)="removeItem($event)"
+            (update)="updateItem($event.item, $event.changes)"
+          >
+          </app-todo-item>
         </li>
       </ul>
     </div>
@@ -18,22 +24,23 @@ import { TodoItem } from '../interfaces/todo-item';
 })
 export class ListManagerComponent implements OnInit {
 
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
+  todoList: TodoItem[];
 
-  constructor() { }
+  constructor(private todoListService: TodoListService) { }
 
   ngOnInit() {
+    this.todoList = this.todoListService.getTodoList();
   }
 
   addItem(title: string) {
-    this.todoList.push({ title });
+    this.todoListService.addItem({ title });
   }
 
+  removeItem(item) {
+    this.todoListService.deleteItem(item);
+  }
+
+  updateItem(item, changes) {
+    this.todoListService.updateItem(item, changes);
+  }
 }
